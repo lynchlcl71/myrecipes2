9e0ed96c4f30fbf7fe337643a2500d737bdfcca2
lynchlcl71 <lynchlcl@aol.com>
2017-04-21 00:15:26 -0400

    add one to many association between recipe and chef

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ApplicationRecord
   validates :name, presence: true
   validates :discription, presence: true, length: { minimum: 5, maximum: 500 }
+  belongs_to :chef
+  validates :chef_id, presence: true
 end 
